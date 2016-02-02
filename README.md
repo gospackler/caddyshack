6e0947bc70e0a7f639938ceeeecbbede45a4295a
johnhof <john.hofrichter@gmail.com>
2016-02-01 23:51:23 -0800

    arch updates

--- README.md
+++ README.md
@@ -5,3 +5,86 @@ Adapter based ORM in Golang
 # DO NOT USE
 
 Architecture not finalized && nothing is implemented...
+
+## Example
+
+Assume directory structure:
+
+```
+- main.go
+- config/
+--- models/
+----- user.json
+----- note.json
+```
+
+#### ./config/models/user.json
+
+```json
+{
+  "adapter": "couchdb",
+  "properties": {
+    "email": {
+      "type": "string",
+      "required": true
+    },
+    "password": {
+      "type": "string",
+      "required": true
+    }
+  }
+}
+```
+
+#### ./config/models/note.json
+
+```json
+{
+  "adapter": "couchdb",
+  "properties": {
+    "title": {
+      "type": "string",
+      "required": true
+    },
+    "contents": {
+      "type": "string",
+      "required": true
+    }
+  }
+}
+```
+
+#### ./main.go
+
+```go
+package main
+
+import (
+    "github.com/bushwood/caddyshack"
+  	couch "github.com/bushwood/caddyshack-couchdb"
+)
+func main() {
+    models, _ := caddyshack.ParseModelDir("./models")
+    collections, _ := caddyshack.LoadModels(models)
+    caddyshack.Connect(&collections)
+    collections["user"].Create(SomeDefinition{})
+
+    // OR
+
+    cs, _ := caddyshack.New()
+    models, _ := caddyshack.ParseModelDir("./models")
+    cs.LoadModels(models)
+    cs.Connect(&collections)
+    cs.Create("user", SomeDefinition{})
+    
+    // OR
+
+    cs, _ := caddyshack.New()
+    models, _ := caddyshack.ParseModelDir("./models")
+    cs.LoadModels(models)
+    cs.Connect(&collections)
+    cs.Collections["user"].Create("user", SomeDefinition{})
+
+
+}
+```
--- parser.go
+++ parser.go
@@ -8,66 +8,73 @@ import (
 )
 
 // ParseModelDir parses a json model files froma  directory into a map
-func ParseModelDir(dir string) (map[string]CollectionSchema, error) {
-    mMap := make(map[string]CollectionSchema)
+func ParseModelDir(dir string) (map[string]CollectionModel, error) {
+    mMap := make(map[string]CollectionModel)
     files, _ := ioutil.ReadDir(dir)
     for _, f := range files {
-        schema, fErr := ParseModelFile(dir + "/" + f.Name())
+        model, fErr := ParseModelFile(dir + "/" + f.Name())
         if fErr != nil {
             return mMap, fErr
         }
         mName := strings.Replace(f.Name(), ".json", "", -1)
-        mMap[mName] = schema
+        mMap[mName] = model
     }
     return mMap, nil
 }
 
 // ParseModelFile parses a json model file into an object
-func ParseModelMapFile(path string) (map[string]CollectionSchema, error) {
+func ParseModelMapFile(path string) (map[string]CollectionModel, error) {
     dat, fErr := ioutil.ReadFile(path)
     if fErr != nil {
-        return map[string]CollectionSchema{}, fErr
+        return map[string]CollectionModel{}, fErr
     }
     return ParseModelMap(dat)
 }
 
 // ParseModelFile parses a json model file into an object
-func ParseModelFile(path string) (CollectionSchema, error) {
+func ParseModelFile(path string) (CollectionModel, error) {
     dat, fErr := ioutil.ReadFile(path)
     if fErr != nil {
-        return CollectionSchema{}, fErr
+        return CollectionModel{}, fErr
     }
 
     return ParseModel(dat)
 }
 
-// ParseModelMap parse a json byte model map into a schema map
-func ParseModelMap(s []byte) (map[string]CollectionSchema, error) {
-    var cs map[string]CollectionSchema
+// ParseModelMap parse a json byte model map into a model map
+func ParseModelMap(s []byte) (map[string]CollectionModel, error) {
+    var cs map[string]CollectionModel
     e := json.Unmarshal(s, &cs)
     return cs, e
 }
 
-// ParseModel parse a model json byte array into a schema
-func ParseModel(m []byte) (CollectionSchema, error) {
-    var cs CollectionSchema
+// ParseModel parse a model json byte array into a model
+func ParseModel(m []byte) (CollectionModel, error) {
+    var cs CollectionModel
     e := json.Unmarshal(m, &cs)
     return cs, e
 }
 
-// LoadSchema load a schema map and return a map of initialized collections
-func LoadSchema(s map[string]CollectionSchema) (map[string]Collection) {
+// LoadModel load a model map and return a map of initialized collections
+func LoadModels(s map[string]CollectionModel) (map[string]Collection) {
     var c map[string]Collection
     // create connection requirements
     return c
 }
 
-type CollectionSchema struct {
-    Adapter string                       `json:adapter`
-    Properties map[string]PropertySchema `json:properties`
+// LoadModel load a model map and return a map of initialized collections
+func LoadModel(s CollectionModel) (Collection) {
+    var c map[string]Collection
+    // create connection requirements
+    return c
 }
 
-type PropertySchema struct {
+type CollectionModel struct {
+    Adapter string                      `json:adapter`
+    Properties map[string]PropertyModel `json:properties`
+}
+
+type PropertyModel struct {
     Type string   `json:type`
     Required bool `json:required`
 }
